eace92dfe6271f63d3919f8f443a94c8d5fe71c0
Yugesh Ralli <yugeshralli@Yugeshs-MacBook-Pro.local>
2025-11-30 07:47:04 +0530

    Fix scroll progress bar z-index to appear above nav

--- components/ScrollProgress.tsx
+++ components/ScrollProgress.tsx
@@ -7,7 +7,7 @@ export const ScrollProgress: React.FC = () => {
     return (
         <motion.div
             style={{ scaleX: scrollYProgress }}
-            className="fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-purple-500 via-pink-500 to-purple-500 origin-left z-50"
+            className="fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-purple-500 via-pink-500 to-purple-500 origin-left z-[100]"
         />
     );
 };
